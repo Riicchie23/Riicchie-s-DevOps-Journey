21756ad4546bfb30424907bbf26407c97cdfda6e
Lorenzo Ricci Alagao <42201290+Riicchie23@users.noreply.github.com>
2024-03-17 19:50:32 +0800

    Added some code on sample.py

--- Python/sample.py
+++ Python/sample.py
@@ -4,6 +4,29 @@
 # Player can know how many hp the boss still has
 # It will be a turn-based type of game
 
+import sys
+
 class RPG():
     def __init__(self):
-        pass
+        boss_hp = None
+        player_hp = None
+        is_active = None #It means the player is active
+    
+    def start_game(self):
+        """Question to start the game
+        """        
+        print("Start game? (y) or (n)")
+        if input() != "y":
+            sys.exit()
+
+
+
+
+
+if __name__ == "__main__":
+    rpg = RPG()
+
+    try:
+        rpg.start_game()
+    except Exception as e:
+        print(e)
