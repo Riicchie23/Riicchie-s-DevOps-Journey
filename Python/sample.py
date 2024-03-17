e45b46c5405e404e456a97669503efd39ec56474
Lorenzo Ricci Alagao <42201290+Riicchie23@users.noreply.github.com>
2024-03-17 12:23:30 +0800

    Update sample.py

--- Python/sample.py
+++ Python/sample.py
@@ -4,11 +4,3 @@
 # Player can know how many hp the boss still has
 # It will be a turn-based type of game
 
-def sampleFunc(num1, num2):
-    """This is used for a sample
-
-    Args:
-        num1 (int): number1
-        num2 (int): number2
-    """    
-
